a152c09b79de52ffbcf0c2645fe8403798665044
koketso-seopela-112 <56113840+koketso-seopela-112@users.noreply.github.com>
2023-10-11 12:24:07 +0200

    Dependency without using injection

--- src/main/java/guru/springframework/spring6di/controllers/MyController.java
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -1,13 +1,21 @@
 package guru.springframework.spring6di.controllers;
 
+import guru.springframework.spring6di.services.GreetingService;
+import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController() {
+        //Concrete implementation of Dependency injection
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHello(){
         System.out.println("Im in the controller");
-
-        return "Hello Everyone!!!";
+        return greetingService.sayGreeting();
     }
 }
