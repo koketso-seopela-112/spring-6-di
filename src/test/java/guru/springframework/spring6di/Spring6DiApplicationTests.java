5301928978806144eb1c1dd44fa4dcf6b8660b53
koketso-seopela-112 <56113840+koketso-seopela-112@users.noreply.github.com>
2023-10-11 11:18:56 +0200

    Test if we can access classes using Application Context on Spring main and test class

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,31 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+
+	@Test
+	void testAutowiredController(){
+		System.out.println(myController.sayHello());
+	}
+	@Test
+	void testGetControllerFromContext(){
+		MyController controller = applicationContext.getBean(MyController.class);
+
+		System.out.println(controller.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
